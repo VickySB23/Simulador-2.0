f7a57cdde8c8a58c3c212e8ef58a318528d60379
Victoria Sanchez B. <168121107+VickySB23@users.noreply.github.com>
2025-11-26 18:11:40 -0300

    11.00

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 # Configurar ruta para encontrar los módulos en 'src'
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 
-# Importamos la interfaz gráfica PRO
+# Importamos la interfaz gráfica PRRRRO
 import gui_pro
 
 if __name__ == "__main__":
